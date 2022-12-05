8a100bcbfcc6befe5401ee32eb70f4cb2f5f30d0
Yaroslava Makedonskaya <yaroslavamakedonskaya@yandex.ru>
2022-12-05 09:08:59 +0300

    done

--- technorabota_final/index.js
+++ technorabota_final/index.js
@@ -1,4 +1,4 @@
-let btn = document.getElementsByClassName('about__map_mini-image')
+let btn = document.querySelectorAll('.about__map_mini-image')
 
 // console.log(btn[0].children);
 
@@ -8,13 +8,39 @@ let mainCard = document.querySelectorAll('.main-img')[1]
 
 let miniCard = document.querySelectorAll('.switch')
 
+// console.dir(mainCard.src);
 
-for (let i = 0; i < btn.length; i++) {
-        btn[i].addEventListener('click', function () {
-            for (const child of btn[i].children) {
-                console.log(child);
-                const originalImg = mainCard.src
-                mainCard.src = child.src 
-                child.src = originalImg
-        }})
-    }
+
+// btn.forEach( el => {
+//     let src = el.children[0].src;
+//     console.log(src);
+//     let alt = el.children[0].alt;
+
+//     // console.dir(el.children[0].src);
+
+//     el.addEventListener('click', function () {
+//         const originalImg = src
+//         mainCard.src = src;    
+//         miniCard = originalImg
+//         // console.dir(el.children[0].src)
+
+//     });
+
+// });
+
+
+
+
+for (let i = 0; i < btn.length; i++) { 
+    btn[i].addEventListener('click', function () {  
+       for (const child of btn[i].children) {
+            console.log(child.alt);
+            const originalImg = mainCard.src
+            // const originalAlt = mainCard.alt
+            mainCard.src = child.src
+            // mainCard.alt = child.alt
+            // child.alt = originalAlt
+            child.src = originalImg
+        }
+    })
+}
